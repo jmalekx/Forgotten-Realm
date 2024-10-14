f850afad5216320e779c4f1ca9033ceb28d9459a
jmalekx <151661788+jmalekx@users.noreply.github.com>
2024-10-14 15:01:18 +0100

    ver 1.1
    * Sprint (no cooldown)
    
    * Sprint with cooldown
    
    * Sprint UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Moving")]
-    public float moveSpeed;
-    public float groundDrag;
 
+    [Header("Keybinds")]
+    public KeyCode jump = KeyCode.Space;
+    public KeyCode sprint = KeyCode.LeftShift;
+
+    [Header("UI")]
+    public Slider sprintSlider;
+
+    [Header("Moving")]
+    private float moveSpeed;
+    public float groundDrag;
+    public float walkSpeed;
+
+    [Header("Sprinting")]
+    public float sprintSpeed;
+    public float sprintDuration;
+    public float sprintCooldown;
+    private float sprintTimer;
+    private bool isCooldown;
+
+    [Header("Jumping")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     bool readyToJump;
 
-    [Header("Keybinds")]
-    public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode sprintKey = KeyCode.LeftControl;
-
-    [Header("Ground")]
+    [Header("Ground Detection")]
     public float playerHeight;
     public LayerMask ground;
     bool grounded;
@@ -29,17 +43,32 @@ public class PlayerController : MonoBehaviour
 
     Vector3 moveDirection;
     Rigidbody rb;
+
+    public Moving state;
+    public enum Moving
+    {
+        walk,
+        sprint,
+        air
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         readyToJump = true;
+        isCooldown = false;
+        sprintTimer = sprintDuration;
+        sprintSlider.maxValue = sprintDuration;
+        sprintSlider.value = sprintDuration;
     }
     void Update()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground); //checking if ground
         KeyboardInputs();
         ControlSpeed();
+        MoveState();
+        UpdateSprintUI();
 
         if (grounded)
             rb.drag = groundDrag;
@@ -56,14 +85,15 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        if (Input.GetKey(jump) && readyToJump && grounded)
         {
             readyToJump = false;
             Jump();
             Invoke(nameof(ResetJump), jumpCooldown);
         }
-    }
 
+    }
+    //-----------------------MOVING
     void MovePlayer()
     {
         moveDirection = playerBody.forward * verticalInput + playerBody.right * horizontalInput; //walk in direction youre looking
@@ -86,14 +116,75 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
+    //-----------------------JUMPING
     void Jump()
     {
-        //resetting y so you can jump exact same height each time
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //resetting y so you can jump exact same height each time
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //impulse cos only applying force once
     }
     void ResetJump()
     {
         readyToJump = true;
+    }
+
+
+    //-----------------------SPRINTING
+    void MoveState()
+    {
+        bool isMoving = horizontalInput != 0 || verticalInput != 0;
+        //sprint
+        if (grounded && Input.GetKey(sprint) && !isCooldown && isMoving)
+        {
+            if (sprintTimer > 0)
+            {
+                state = Moving.sprint;
+                moveSpeed = sprintSpeed;
+                sprintTimer -= Time.deltaTime;
+                Debug.Log("Sprinting! Timer: " + sprintTimer);
+            }
+            else
+            {
+                isCooldown = true;
+                Invoke(nameof(ResetCooldown), sprintCooldown);
+                state = Moving.walk;
+                moveSpeed = walkSpeed;
+                Debug.Log("Sprint on cooldown!");
+            }
+        }
+        //walk
+        else if (grounded)
+        {
+            state = Moving.walk;
+            moveSpeed = walkSpeed;
+        }
+        //air
+        else
+        {
+            state = Moving.air;
+        }
+        
+    }
+    void ResetCooldown()
+    {
+        isCooldown = false;
+        sprintTimer = sprintDuration;
+    }
+    void UpdateSprintUI()
+    {
+        if (!isCooldown)
+        {
+            sprintSlider.value = sprintTimer;
+        }
+        else
+        {
+            float refillSpeed = sprintDuration / sprintCooldown; //bar refilling based on cooldown
+            sprintSlider.value += refillSpeed * Time.deltaTime;
+           
+            if (sprintSlider.value >= sprintDuration) //prevent overfill
+            {
+                sprintSlider.value = sprintDuration;
+            }
+        }
     }
 }
