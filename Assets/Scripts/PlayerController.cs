c89bd05c20f8c564b5dc12ddd0f090661957fec9
Ben <ben.gauntt10@gmail.com>
2024-10-16 15:14:29 +0100

    change

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    PlayerInput playerInput;
+    PlayerInput.MainActions input;
+    CharacterController controller;
+    Animator animator;
 
-    [Header("Keybinds")]
-    public KeyCode jump = KeyCode.Space;
-    public KeyCode sprint = KeyCode.LeftShift;
+   // [Header("Keybinds")]
+  //  public KeyCode jump = KeyCode.Space;
+   // public KeyCode sprint = KeyCode.LeftShift;
+  //  public KeyCode attack = KeyCode.Mouse0;
 
     [Header("UI")]
     public Slider sprintSlider;
+
 
     [Header("Moving")]
     private float moveSpeed;
     public float groundDrag;
     public float walkSpeed;
-
+    [Header("Camera")]
+    public Camera cam;
     [Header("Sprinting")]
     public float sprintSpeed;
     public float sprintDuration;
@@ -61,14 +69,58 @@ public class PlayerController : MonoBehaviour
         sprintTimer = sprintDuration;
         sprintSlider.maxValue = sprintDuration;
         sprintSlider.value = sprintDuration;
+        
+    }
+    void Awake()
+    { 
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        
+
+        playerInput = new PlayerInput();
+        input = playerInput.Main;
+        AssignInputs();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //resetting y so you can jump exact same height each time
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //impulse cos only applying force once
+    }
+    void JumpAttempt(){
+        if (readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+    void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    void AssignInputs(){
+        input.Jump.performed += ctx => JumpAttempt();
+        input.Attack.started += ctx => Attack();
+        input.Sprint.performed += ctx => SprintStart();
+       
     }
     void Update()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground); //checking if ground
+     
         KeyboardInputs();
         ControlSpeed();
         MoveState();
         UpdateSprintUI();
+        if(input.Attack.IsPressed())
+        { Attack();}
+
+        
 
         if (grounded)
             rb.drag = groundDrag;
@@ -85,12 +137,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(jump) && readyToJump && grounded)
-        {
-            readyToJump = false;
-            Jump();
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
+
 
     }
     //-----------------------MOVING
@@ -118,23 +165,29 @@ public class PlayerController : MonoBehaviour
 
 
     //-----------------------JUMPING
-    void Jump()
-    {
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //resetting y so you can jump exact same height each time
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //impulse cos only applying force once
-    }
-    void ResetJump()
-    {
-        readyToJump = true;
-    }
 
 
     //-----------------------SPRINTING
     void MoveState()
     {
+
+        //walk
+        if (grounded)
+        {
+            state = Moving.walk;
+            moveSpeed = walkSpeed;
+        }
+        //air
+        else
+        {
+            state = Moving.air;
+        }
+        
+    }
+    void SprintStart(){
         bool isMoving = horizontalInput != 0 || verticalInput != 0;
         //sprint
-        if (grounded && Input.GetKey(sprint) && !isCooldown && isMoving)
+        if (grounded && !isCooldown && isMoving)
         {
             if (sprintTimer > 0)
             {
@@ -152,18 +205,6 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("Sprint on cooldown!");
             }
         }
-        //walk
-        else if (grounded)
-        {
-            state = Moving.walk;
-            moveSpeed = walkSpeed;
-        }
-        //air
-        else
-        {
-            state = Moving.air;
-        }
-        
     }
     void ResetCooldown()
     {
@@ -187,4 +228,69 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+
+
+
+
+
+    
+    [Header("Attacking")]
+    public float attackDistance = 3f;
+    public float attackDelay = 0.4f;
+    public float attackSpeed = 1f;
+    public int attackDamage = 1;
+    public LayerMask attackLayer;
+
+    public GameObject hitEffect;
+  
+
+    bool attacking = false;
+    bool readyToAttack = true;
+    int attackCount;
+
+    public void Attack()
+    {
+        if(!readyToAttack || attacking) return;
+
+        readyToAttack = false;
+        attacking = true;
+
+        Invoke(nameof(ResetAttack), attackSpeed);
+        Invoke(nameof(AttackRaycast), attackDelay);
+
+
+        if(attackCount == 0)
+        {
+            
+            attackCount++;
+        }
+        else
+        {
+           
+            attackCount = 0;
+        }
+    }
+
+    void ResetAttack()
+    {
+        attacking = false;
+        readyToAttack = true;
+    }
+
+    void AttackRaycast()
+    {
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, attackDistance, attackLayer))
+        { 
+            HitTarget(hit.point);
+
+        } 
+    }
+
+    void HitTarget(Vector3 pos)
+    {
+
+        GameObject GO = Instantiate(hitEffect, pos, Quaternion.identity);
+        Destroy(GO, 20);
+    }
 }
+
