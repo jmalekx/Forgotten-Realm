3de4cbdb83c80047f068efa61a0801e9f93294be
darenagos <103580328+darenagos@users.noreply.github.com>
2024-10-22 11:27:36 +0100

    added dagger prefab

---
+++ Assets/Scripts/Inventory/Dagger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dagger : Collectible
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Dagger instance created: " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
