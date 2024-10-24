1418d7da6d4628273bd9a4574acbf9a090c912d0
jmalekx <151661788+jmalekx@users.noreply.github.com>
2024-10-24 14:47:32 +0100

    Updated Inventory UI + Organised Folders more
    * Updated UI Text
    
    * Updated UI anchors
    
    * Imported MushroomMan
    
    not into the scene tho
    
    * Folder Organise
    
    * Apple model
    
    * Added Apple Icon for Inventory
    
    * Add Icon to inventory
    
    * ItemUI Updated
    
    -will show icon if icon present, else just text (so can see as we make icons)
    
    -shows all item slots even if empty and shows visual change on selected item slot
    
    * Adjusted Tint Overlay Value
    
    * Organised Folder

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -6,14 +6,29 @@ using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour
 {
-    public TMP_Text itemNameText;
+    public Image iconImage;
+    public Image tintOverlay;
     public TMP_Text countText;
+    public TMP_Text itemNameText;
     public void UpdateSlot(ItemData itemData)
     {
         if (itemData != null)
         {
-            itemNameText.text = itemData.itemName;
+            iconImage.sprite = itemData.icon;
             countText.text = itemData.count > 1 ? itemData.count.ToString() : ""; // Set item count
+            iconImage.enabled = true;
+            tintOverlay.enabled = false;
+
+            if (itemData.icon == null)
+            {
+                itemNameText.text = itemData.itemName; //show item name
+                itemNameText.enabled = true;
+            }
+            else
+            {
+                itemNameText.enabled = false;
+                iconImage.color = new Color(1.5f, 1.5f, 1.5f, 1.5f);
+            }
         }
         else
         {
@@ -24,7 +39,24 @@ public class ItemSlot : MonoBehaviour
     // Clear the slot when no item is assigned
     public void ClearSlot()
     {
-        itemNameText.text = "";
+        iconImage.sprite = null;
+        iconImage.color = new Color(0f, 0f, 0f, 0f);
         countText.text = "";
+        itemNameText.enabled = false;
+        tintOverlay.enabled = false;
+    }
+
+    public void SetSelected(bool isSelected)
+    {
+        if (isSelected)
+        {
+            tintOverlay.enabled = true;
+            tintOverlay.color = new Color(1f, 0f, 1f, 0.2f);
+ 
+        }
+        else
+        {
+            tintOverlay.enabled = false;
+        }
     }
 }
