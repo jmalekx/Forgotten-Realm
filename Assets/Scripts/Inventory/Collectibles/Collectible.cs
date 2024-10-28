91695a8bb183fc4f053a3dd8101c5184cd44e576
jmalekx <151661788+jmalekx@users.noreply.github.com>
2024-10-28 22:03:00 +0000

    Pickup System
    * Changed to press E to pickup
    
    -no more collider system, but press e to pickup (unity input system)

--- Assets/Scripts/Inventory/Collectibles/Collectible.cs
+++ Assets/Scripts/Inventory/Collectibles/Collectible.cs
@@ -12,16 +12,16 @@ public class Collectible : MonoBehaviour
 {
     public ItemData collectibleItemData; // Reference to the item's data
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Collectible: OnTriggerEnter called"); // Log when the trigger is activated
-        if (other.CompareTag("Player")) // Check if the colliding object is tagged as "Player"
-        {
-            Debug.Log("Player collided with collectible: " + collectibleItemData.itemName); // Log the item's name
-            Inventory.Instance.AddItem(collectibleItemData); // Add the item to the inventory
-            Destroy(gameObject); // Destroy the collectible object
-        }
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("Collectible: OnTriggerEnter called"); // Log when the trigger is activated
+    //    if (other.CompareTag("Player")) // Check if the colliding object is tagged as "Player"
+    //    {
+    //        Debug.Log("Player collided with collectible: " + collectibleItemData.itemName); // Log the item's name
+    //        Inventory.Instance.AddItem(collectibleItemData); // Add the item to the inventory
+    //        Destroy(gameObject); // Destroy the collectible object
+    //    }
+    //}
 }
 
 
